7e83cc2fa238e97fb46481e593b06d22be714d67
miki kazuo <c501506022@tokushima-u.ac.jp>
2016-11-21 00:31:06 +0900

    ステージ選択画面途中・ステージのskybox変更

--- Assets/Scripts/stage_select/stage_select.cs
+++ Assets/Scripts/stage_select/stage_select.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class stage_select : MonoBehaviour
 {
 
+    public GameObject[] arrow;
     //ステージ
     //以下の順に入れ子構造にすること
     private enum Stage
@@ -32,76 +33,129 @@ public class stage_select : MonoBehaviour
         none
     }
 
-    private Dir nowdir=Dir.none;
-    private Stage stage_is=0;
-    Transform []stage;
+    private Dir nowdir = Dir.none;
+    private Stage stage_is = 0;
+    private bool moving;
+
+    Transform[] stage;
     // Use this for initialization
     void Start()
     {
-        stage= new Transform[(int)Stage.None];
+        stage = new Transform[(int)Stage.None + 1];
         // 子要素を全て取得する
         for (int i = 0; i < transform.childCount; i++)
         {
 
             stage[i] = transform.GetChild(i);
         }
-        stage[(int)stage_is].gameObject.SetActive(true);
+        // stage[(int)stage_is].gameObject.SetActive(true);
+        Vector2 posi = stage[(int)stage_is].GetComponent<RectTransform>().anchoredPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        switch (nowdir)
+
+
+
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            case Dir.none:
-               
-                if (Input.GetKeyDown(KeyCode.Return))
-                {
-                    switch (stage_is)
-                    {
-                        case Stage.tutorial:
-                            break;
-                        case Stage.stage1:
-                            break;
-                        case Stage.stage2:
-                            break;
-                        case Stage.stage3:
-                            break;
-                        case Stage.stage4:
-                            break;
-                        case Stage.stage5:
-                            break;
-                    }
-                    SceneManager.LoadScene("main");
-                }
-                if (stage_is<Stage.None-1&&Input.GetKeyDown(KeyCode.RightArrow))
-                {
-                    nowdir = Dir.righting;
+            switch (stage_is)
+            {
+                case Stage.tutorial:
+                    break;
+                case Stage.stage1:
+                    break;
+                case Stage.stage2:
+                    break;
+                case Stage.stage3:
+                    break;
+                case Stage.stage4:
+                    break;
+                case Stage.stage5:
+                    break;
+            }
+            SceneManager.LoadScene("main");
+        }
+        Vector2 samp = stage[(int)stage_is].GetComponent<RectTransform>().anchoredPosition;
+        samp.y += 20;
+        if (stage[6].GetComponent<RectTransform>().anchoredPosition.x < samp.x - 15 ||
+            stage[6].GetComponent<RectTransform>().anchoredPosition.x > samp.x + 15 ||
+                   stage[6].GetComponent<RectTransform>().anchoredPosition.y < samp.y - 15 ||
+                                stage[6].GetComponent<RectTransform>().anchoredPosition.y > samp.y + 15)
+        {
+            Vector2 point_sa;
+            point_sa.x = samp.x - stage[6].GetComponent<RectTransform>().anchoredPosition.x;
+            point_sa.y = samp.y - stage[6].GetComponent<RectTransform>().anchoredPosition.y;
+
+            float radi = Mathf.Atan2(point_sa.y, point_sa.x);
+       
+            Vector2 posi = stage[6].GetComponent<RectTransform>().anchoredPosition;
+            posi.x += Mathf.Cos(radi) * Time.deltaTime * 100;
+            posi.y += Mathf.Sin(radi) * Time.deltaTime * 100;
+
+            stage[6].GetComponent<RectTransform>().anchoredPosition = posi;
+            moving = true;
+        }
+        else
+            moving = false;
+        //Vector2 posi= stage[(int)stage_is].GetComponent<RectTransform>().anchoredPosition;
+        //posi.x -= 15;
+        //posi.y += 10;
+
+        //stage[6].GetComponent<RectTransform>().anchoredPosition = posi;
+        arrow[0].SetActive(false);
+        arrow[1].SetActive(false);
+        arrow[2].SetActive(false);
+        if (!moving)
+        switch (stage_is)
+        {
+            case Stage.tutorial:
+                    arrow[0].SetActive(true);
+                if (Input.GetKeyDown(KeyCode.RightArrow))
                     stage_is++;
-                    break;
-                }
-                else if (stage_is > Stage.tutorial && Input.GetKeyDown(KeyCode.LeftArrow))
-                {
-                    nowdir = Dir.lefting;
+                break;
+            case Stage.stage1:
+                    arrow[0].SetActive(true);
+                    arrow[1].SetActive(true);
+                    if (Input.GetKeyDown(KeyCode.RightArrow))
+                    stage_is++;
+                else if (Input.GetKeyDown(KeyCode.LeftArrow))
                     stage_is--;
-                    break;
-                }
+                break;
+            case Stage.stage2:
+                    arrow[0].SetActive(true);
+                    arrow[1].SetActive(true);
+
+                    if (Input.GetKeyDown(KeyCode.RightArrow))
+                    stage_is++;
+                else if (Input.GetKeyDown(KeyCode.LeftArrow))
+                    stage_is--;
+                break;
+            case Stage.stage3:
+                    arrow[1].SetActive(true);
+                    arrow[2].SetActive(true);
+                    if (Input.GetKeyDown(KeyCode.LeftArrow))
+                    stage_is++;
+                else if (Input.GetKeyDown(KeyCode.UpArrow))
+                    stage_is--;
 
                 break;
-            case Dir.righting:
-            case Dir.lefting:
-                for (int i = 0; i < transform.childCount; i++)
-                {
-                    stage[i].gameObject.SetActive(false);
-                }
-
-                stage[(int)stage_is].gameObject.SetActive(true);
-                nowdir = Dir.none;
-                    break;
-
-
+            case Stage.stage4:
+                    arrow[0].SetActive(true);
+                    arrow[1].SetActive(true);
+                    if (Input.GetKeyDown(KeyCode.RightArrow))
+                    stage_is--;
+                else if (Input.GetKeyDown(KeyCode.LeftArrow))
+                    stage_is++;
+                break;
+            case Stage.stage5:
+                    arrow[0].SetActive(true);
+                    if (Input.GetKeyDown(KeyCode.RightArrow))
+                    stage_is--;
+                break;
         }
+
 
     }
 
